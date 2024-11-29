2d0d67f9e95a6dc301ac442c097a68cb36d1c5ce
Vlad <vaaoala3@gmail.com>
2024-11-29 03:39:27 +0300

    add queue

--- include/cycle_queue.h
+++ include/cycle_queue.h
@@ -3,14 +3,89 @@
 
 #include <deque>
 
+template <class T>
 class Queue {
+	T* data;
+	size_t size;
+	size_t cap;
+	size_t head_ind;
+	size_t tail_ind;
 
-
+	void repack() {
+		T* tmp = new T[2 * cap];
+		for (int i = head_ind; i < cap; i++)
+			tmp[i - head_ind] = data[i];
+		if (tail_ind < head_ind)
+			for (int i = cap - head_ind; i <= tail_ind; i++)
+				tmp[i] = data[i - cap + head_ind];
+		head_ind = 0;
+		tail_ind = size - 1;
+		delete[] data;
+		cap *= 2;
+		data = tmp;
+	}
+	
 public:
+	Queue(): size(0), cap(9), head_ind(0), tail_ind(-1){
+		data = new T[cap];
+	}
+	
+	void push(const T& val) {
+		if (size == cap)
+			this->repack();
+		tail_ind++;
+		if (tail_ind >= cap)
+			data[tail_ind - cap] = val;
+		else
+			data[tail_ind] = val;
+		size++;
+	}
 
+	void pop() {
+		if (!empty()) {
+			head_ind++;
+			size--;
+		}
+		else
+			throw "Incorrect size";
 
+	}
 
+	T first() const {
+		if (!empty())
+			return data[head_ind];
+		else
+			throw "Incorrect size";
+	}
 
+	T back() const {
+		if (!empty())
+			return data[tail_ind];
+		else
+			throw "Incorrect size";
+	}
+
+	bool empty() const {
+		return size == 0;
+	}
+
+	bool full() {
+		return size == cap;
+	}
+
+	size_t get_size() const {
+		return size;
+	}
+
+	void clear() {
+		size = 0;
+		head_ind = 0;
+		tail_ind = -1;
+		delete[] data;
+		data = new T[cap];
+	}
+
+	~Queue() { delete[] data; }
 };
 
 #endif
--- test/test_cycle_queue.cpp
+++ test/test_cycle_queue.cpp
@@ -1,2 +1,136 @@
 #include "cycle_queue.h"
 #include <gtest.h>
+
+TEST(Queue, can_create_queue)
+{
+    ASSERT_NO_THROW(Queue<int> q);
+}
+
+TEST(Queue, can_get_first_elem_from_queue)
+{
+    Queue<int> q;
+
+    q.push(1);
+    q.push(2);
+
+    ASSERT_EQ(q.first(), 1);
+}
+
+TEST(Queue, can_get_first_elem_from_empty_queue)
+{
+    Queue<int> q;
+
+    ASSERT_ANY_THROW(q.first());
+}
+
+
+TEST(Queue, can_get_last_elem_from_queue)
+{
+    Queue<int> q;
+
+    q.push(1);
+    q.push(2);
+
+    ASSERT_EQ(q.back(), 2);
+}
+
+TEST(Queue, can_get_last_elem_from_empty_queue)
+{
+    Queue<int> q;
+
+    ASSERT_ANY_THROW(q.back());
+}
+
+TEST(Queue, can_check_empty_queue)
+{
+    Queue<int> q;
+
+    ASSERT_TRUE(q.empty());
+}
+
+TEST(Queue, can_check_not_empty_queue)
+{
+    Queue<int> q;
+    q.push(1);
+
+    ASSERT_FALSE(q.empty());
+}
+
+TEST(Queue, can_check_full_queue)
+{
+    Queue<int> q;
+
+    for (int i = 0; i < 9; i++)
+        q.push(i);
+
+    ASSERT_TRUE(q.full());
+}
+
+TEST(Queue, can_check_not_full_queue)
+{
+    Queue<int> q;
+
+    q.push(1);
+
+    ASSERT_FALSE(q.full());
+    ASSERT_FALSE(q.full());
+}
+
+TEST(Queue, can_several_elems_push_to_queue) {
+    Queue<int> q;
+    q.push(1);
+    q.push(2);
+
+    ASSERT_EQ(q.get_size(), 2);
+    ASSERT_EQ(q.first(), 1);
+    ASSERT_EQ(q.back(), 2);
+
+}
+
+TEST(Queue, can_pop_elems_from_queue) {
+    Queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    
+    q.pop();
+    q.pop();
+    q.pop();
+
+    ASSERT_TRUE(q.empty());
+    ASSERT_ANY_THROW(q.first());
+}
+
+TEST(Queue, can_pop_elems_from_empty_queue) {
+    Queue<int> q;
+
+    ASSERT_ANY_THROW(q.pop());
+}
+
+
+TEST(Queue, can_repack_queue) {
+    Queue<int> q;
+    for (int i = 0; i < 10; i++) 
+        q.push(i);
+
+    ASSERT_FALSE(q.full());
+}
+
+TEST(queue, can_get_queue_size) {
+    Queue<int> q;
+    q.push(1);
+    q.push(1);
+
+    ASSERT_EQ(q.get_size(), 2);
+}
+
+TEST(Queue, can_clear_queue) {
+    Queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+
+    q.clear();
+    
+    ASSERT_TRUE(q.empty());
+}
